44fe96e0a6066b788b3f1914bab848183061773b
Sebastian Steinau <32056906+ssteinau@users.noreply.github.com>
2026-09-14 17:46:47 +0200

    submit file

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+using Recite.Core.Citations;
+
+var doc = ExampleData.SampleDocument();
+
+foreach (var template in ExampleData.Templates)
+    Console.WriteLine($"{template,-24} -> {new CitationKeyScheme(template).BaseKey(doc)}");
